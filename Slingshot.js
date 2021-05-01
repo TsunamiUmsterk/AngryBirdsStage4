696a79cbec891fba7176a80d5175e75c012dcbb7
TsunamiUmsterk <mmb3391@gmail.com>
2021-05-01 13:19:33 +0100

    added catapult

--- Slingshot.js
+++ Slingshot.js
@@ -9,6 +9,9 @@ class SlingShot{
         this.pointB = pointB
         this.sling = Constraint.create(options);
         World.add(world, this.sling);
+        this.sling1 = loadImage("sprites/sling1.png");
+        this.sling2 = loadImage("sprites/sling2.png");
+        this.sling3 = loadImage("sprites/sling3.png");
     }
 
     fly(){
@@ -16,11 +19,24 @@ class SlingShot{
     }
 
     display(){
+        image(this.sling1, 270, 70, 38, 150);
+        image(this.sling2, 240, 80, 42, 80);
+
         if(this.sling.bodyA){
             var pointA = this.sling.bodyA.position;
             var pointB = this.pointB;
-            strokeWeight(4);
-            line(pointA.x, pointA.y, pointB.x, pointB.y);
+            strokeWeight(10);
+            stroke(48, 22, 8);
+            if(pointA.x < 280) {
+                line(pointA.x - 25, pointA.y, pointB.x + 25, pointB.y - 5);
+                line(pointA.x - 25, pointA.y, pointB.x - 25, pointB.y - 5);
+                image(this.sling3, pointA.x - 25, pointA.y - 10, 10, 20);
+            } else {
+                line(pointA.x + 25, pointA.y, pointB.x + 25, pointB.y - 5);
+                line(pointA.x + 25, pointA.y, pointB.x - 25, pointB.y - 5);
+                image(this.sling3, pointA.x + 25, pointA.y - 10, 10, 20);
+            }
+            
         }
     }
     
